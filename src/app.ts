44350b238bf4582b38c12eb7b45ecf38023a192f
codersrabbi <pcrabbi2020@gmail.com>
2026-07-17 17:25:33 +0600

    routes refector

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { UserRouters } from "./app/modules/users/user.route";
 import { NewsRouter } from "./app/modules/news/news.route";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notfound";
+import router from "./app/routes";
 
 export const app: Application = express();
 export const port = 3000;
@@ -13,9 +14,7 @@ export const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/reporters", ReporterRoutes);
-app.use("/api/v1/users", UserRouters);
-app.use("/api/v1/news", NewsRouter);
+app.use("/api/v1", router);
 app.get("/", (req: Request, res: Response) => {
   res.send("Gen Voice Server is running...");
 });
---
+++ src/app/routes/index.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { ReporterRoutes } from "../modules/reporters/reporter.route";
+import { UserRouters } from "../modules/users/user.route";
+import { NewsRouter } from "../modules/news/news.route";
+
+const router = Router();
+
+const modulesRoutes = [
+  {
+    path: "/users",
+    route: ReporterRoutes,
+  },
+  {
+    path: "/reporters",
+    route: UserRouters,
+  },
+  {
+    path: "/news",
+    route: NewsRouter,
+  },
+];
+
+modulesRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
