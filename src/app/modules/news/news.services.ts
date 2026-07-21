9ddbc226b5de0b3e2a989ea65732bf4a7af70c9b
codersrabbi <pcrabbi2020@gmail.com>
2026-07-21 16:00:57 +0600

    search method using builderQuery

--- src/app/modules/news/news.services.ts
+++ src/app/modules/news/news.services.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import AppError from "../../error/AppError";
 import { TNews } from "./news.interface";
 import { News } from "./news.model";
+import QueryBuilder from "../../builder/QueryBuilder";
 
 const createNewsIntoDB = async (newsData: TNews) => {
   const result = await News.create(newsData);
@@ -9,67 +10,85 @@ const createNewsIntoDB = async (newsData: TNews) => {
 };
 
 const getAllNewsFromDB = async (query: Record<string, unknown>) => {
-  const queryObj = { ...query };
-  console.log(query);
-  let searchTerm = "";
-  if (query?.searchTerm) {
-    searchTerm = query.searchTerm as string;
-  }
+  // const queryObj = { ...query };
+  // console.log(query);
+  // let searchTerm = "";
+  // if (query?.searchTerm) {
+  //   searchTerm = query.searchTerm as string;
+  // }
 
-  const searchQuery = News.find({
-    $or: ["title", "shortDetails", "content", "location", "tags"].map(
-      (field) => ({
-        [field]: { $regex: searchTerm, $options: "i" },
-      }),
-    ),
-  });
+  // const searchQuery = News.find({
+  //   $or: ["title", "shortDetails", "content", "location", "tags"].map(
+  //     (field) => ({
+  //       [field]: { $regex: searchTerm, $options: "i" },
+  //     }),
+  //   ),
+  // });
 
-  const excludeFields = ["searchTerm", "sort", "limit", "page", "fields"];
-  excludeFields.forEach((el) => delete queryObj[el]);
+  // const excludeFields = ["searchTerm", "sort", "limit", "page", "fields"];
+  // excludeFields.forEach((el) => delete queryObj[el]);
 
-  // const result = await searchQuery
+  // // const result = await searchQuery
+  // //   .populate("reporterId")
+  // //   .populate("approvedBy");
+  // // .populate("categoryId");
+  // // return result;
+
+  // const filterQuery = searchQuery.find(queryObj);
+
+  // let sort = "createdAt";
+  // if (query.sort) {
+  //   sort = query.sort as string;
+  // }
+
+  // const sortQuery = filterQuery.sort(sort);
+
+  // let page = 1;
+  // let limit = 1;
+  // let skip = 0;
+
+  // if (query.limit) {
+  //   limit = Number(query.limit);
+  // }
+
+  // if (query.page) {
+  //   page = Number(query.page);
+  //   skip = (page - 1) * limit;
+  // }
+
+  // const paginateQuery = sortQuery.skip(skip);
+
+  // const limitQuery = paginateQuery.limit(limit);
+
+  // let fields = "-__v";
+  // if (query.fields) {
+  //   fields = (query.fields as string).split(",").join(" ");
+  // }
+
+  // const filedsQuery = await limitQuery
+  //   .select(fields)
   //   .populate("reporterId")
   //   .populate("approvedBy");
-  // .populate("categoryId");
-  // return result;
 
-  const filterQuery = searchQuery.find(queryObj);
+  // return filedsQuery;
 
-  let sort = "createdAt";
-  if (query.sort) {
-    sort = query.sort as string;
-  }
+  const searchAbleFiends = [
+    "title",
+    "shortDetails",
+    "content",
+    "location",
+    "tags",
+  ];
 
-  const sortQuery = filterQuery.sort(sort);
+  const newsQuery = new QueryBuilder(News.find(), query)
+    .search(searchAbleFiends)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
 
-  let page = 1;
-  let limit = 1;
-  let skip = 0;
-
-  if (query.limit) {
-    limit = Number(query.limit);
-  }
-
-  if (query.page) {
-    page = Number(query.page);
-    skip = (page - 1) * limit;
-  }
-
-  const paginateQuery = sortQuery.skip(skip);
-
-  const limitQuery = paginateQuery.limit(limit);
-
-  let fields = "-__v";
-  if (query.fields) {
-    fields = (query.fields as string).split(",").join(" ");
-  }
-
-  const filedsQuery = await limitQuery
-    .select(fields)
-    .populate("reporterId")
-    .populate("approvedBy");
-
-  return filedsQuery;
+  const result = await newsQuery.modelQuery;
+  return result;
 };
 
 const getSingleNewsFromDB = async (id: string) => {
